fdc105c4d71735f2438ed723aeb13a8e671871ad
이현종 <59433342+dlguswhd@users.noreply.github.com>
2026-01-03 11:06:30 +0900

    12주차
    12주차

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+	[SerializeField]
+	private Gun currentGun;
+
+	private float currentFireRate;
+
+	private bool isReload = false;
+
+	private AudioSource audioSource;
+
+	void Start()
+    {
+		audioSource = GetComponent<AudioSource>();
+    }
+
+	void Update () {
+
+		GunFireRateCalc();
+		TryFire();
+
+	}
+
+	private void GunFireRateCalc()
+    {
+		if (currentFireRate > 0)
+			currentFireRate -= Time.deltaTime;
+    }
+
+	private void TryFire()
+    {
+		if (Input.GetButton("Fire1") && currentFireRate <= 0 && !isReload)
+        {
+			Fire();
+        }
+    }
+
+	private void Fire()
+    {
+		if(!isReload)
+        {
+			if (currentGun.currentBulletCount > 0)
+				Shoot();
+			else
+				StartCoroutine(ReloadCoroutine());
+		}
+    }
+
+	private void Shoot()
+    {
+		currentGun.currentBulletCount--;
+		currentFireRate = currentGun.fireRate;
+		PlaySE(currentGun.fire_Sound);
+		currentGun.muzzleFlash.Play();
+		Debug.Log("총알 발사함");
+    }
+
+	IEnumerator ReloadCoroutine()
+    {
+		if(currentGun.carryBulletCount > 0)
+        {
+			currentGun.anim.SetTrigger("Reload");
+
+			isReload = true;
+			yield return new WaitForSeconds(currentGun.reloadTime);
+
+			if(currentGun.carryBulletCount >= currentGun.reloadBulletCount)
+            {
+				currentGun.currentBulletCount = currentGun.reloadBulletCount;
+				currentGun.carryBulletCount -= currentGun.reloadBulletCount;
+            }
+			else
+            {
+				currentGun.currentBulletCount = currentGun.carryBulletCount;
+				currentGun.carryBulletCount = 0;
+            }
+
+			isReload = false;
+
+        }
+    }
+
+	private void PlaySE(AudioClip _clip)
+    {
+		audioSource.clip = _clip;
+		audioSource.Play();
+	}
+}
